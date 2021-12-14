9bddc2b0e3f0c0a3c08016b9e588cdd4237a80ce
Gabriel Fonseca <gabriel.coutinho@e2x.com>
2021-12-14 12:45:13 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import ToggleComponent from "./useToggle/ToggleComponent";
+import UpdateEffectComponent from "./useUpdateEffect/UpdateEffectComponent";
+import ArrayComponent from "./useArray/ArrayComponent";
+import PreviousComponent from "./usePrevious/PreviousComponent";
+
+function App() {
+  // return <ToggleComponent />
+  // return <TimeoutComponent />
+  // return <DebounceComponent />
+  // return <UpdateEffectComponent />
+  // return <ArrayComponent />;
+  return <PreviousComponent />;
+}
+
+export default App;
